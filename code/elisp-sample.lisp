b06681bccc024f1556e078ebcd952eac437312af
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2024-08-21 14:23:30 +0200

    slides-updated

--- code/elisp-sample.lisp
+++ code/elisp-sample.lisp
@@ -1,4 +1,6 @@
+- Use Ctrl-w followed by h, j, k, or l to navigate between splits.
 (defun count-words-in-buffer ()
+- Use Ctrl-w followed by h, j, k, or l to navigate between splits.
   "Count the number of words in the current buffer."
   (interactive)
   (save-excursion
---
+++ code/tmux-256color.md
@@ -0,0 +1,2 @@
+[This](https://stackoverflow.com/questions/10158508/lose-vim-colorscheme-in-tmux-mode) stack overflow post to see the tmux color mode to xterm and stuff.
+
